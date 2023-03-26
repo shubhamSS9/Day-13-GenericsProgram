e21f8ed3048b8f3f227cd4105674f5b606379e39
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-26 17:31:30 +0530

    UC2_FindMax_Float

--- src/com/bridglabz/GenericsProgram.java
+++ src/com/bridglabz/GenericsProgram.java
@@ -1,28 +1,49 @@
 package com.bridglabz;
 
 public class GenericsProgram {
-    public void FindMaxInt(Integer a, Integer b, Integer c) {
-        int max = a;
-        if ((b.compareTo(max)) > 0) {
-            max = b;
+//    public void FindMaxInt(Integer a, Integer b, Integer c) {
+//        int max = a;
+//        if ((b.compareTo(max)) > 0) {
+//            max = b;
+//        }
+//        if ((c.compareTo(max)) > 0) {
+//            max = c;
+//        }
+//        System.out.println("Maximum of " + a + "," + b + " and " + c + " is: " + max);
+//
+//    }
+    public void FindMaxFloat(Float a, Float b, Float c){
+        float max = a;
+        if((b.compareTo(max))>0)
+        {
+            max=b;
         }
-        if ((c.compareTo(max)) > 0) {
-            max = c;
+        if((c.compareTo(max))>0){
+            max=c;
         }
-        System.out.println("Maximum of " + a + "," + b + " and " + c + " is: " + max);
-
+        System.out.println("Maximum of "+a+ ","+b+" and "+c+ " is: "+max);
     }
 
     public static void main(String[] args) {
         GenericsProgram genericsProgram = new GenericsProgram();
-        Integer a = 10, b = 5, c = 3;
-        System.out.println("********  Integer values  ********");
+//        Integer a = 10, b = 5, c = 3;
+        Float d = 10.5F ,e=5.5F,f=3.3F;
+//        System.out.println("********  Integer values  ********");
+//        // test case 1 :- Given Max Number at 1st Position return the Same Number
+//        genericsProgram.FindMaxInt(a, b, c);
+//        // test case 2 :- Given Max Number at 2nd Position return the Same Number
+//        genericsProgram.FindMaxInt(b, a, c);
+//        // test case 3 :- Given Max Number at 3rd Position return the Same Number
+//        genericsProgram.FindMaxInt(c, b, a);
+
+        System.out.println("********  Float values  ********");
         // test case 1 :- Given Max Number at 1st Position return the Same Number
-        genericsProgram.FindMaxInt(a, b, c);
+        genericsProgram.FindMaxFloat(d,e,f);
         // test case 2 :- Given Max Number at 2nd Position return the Same Number
-        genericsProgram.FindMaxInt(b, a, c);
+        genericsProgram.FindMaxFloat(e,d,f);
         // test case 3 :- Given Max Number at 3rd Position return the Same Number
-        genericsProgram.FindMaxInt(c, b, a);
+        genericsProgram.FindMaxFloat(d,f,d);
+
 
     }
 }
